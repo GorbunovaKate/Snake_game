d8a3279e9cc5614068e502eaffb33a1d9b2a286b
Gorbunova Ekaterina <login.kate1.pass@gmail.com>
2023-04-20 16:32:07 +0700

    Initial snake game

---
+++ app/src/main/java/ru/gorbunova/snake/MainActivity.kt
@@ -0,0 +1,42 @@
+package ru.gorbunova.snake
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.gorbunova.snake.ui.theme.SnakeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SnakeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Snake()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Snake() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SnakeTheme {
+        Snake()
+    }
+}
